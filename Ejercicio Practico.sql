a57c8f32446fbe1ce40580fee70a032d5f0bd7b6
Adiel Montepeque <137818505+AdielMontepeque1@users.noreply.github.com>
2024-02-28 20:21:43 -0600

    Update Ejercicio Practico.sql

--- Ejercicio Practico.sql
+++ Ejercicio Practico.sql
@@ -3,7 +3,7 @@ CREATE DATABASE ejercicio1x;
 USE ejercicio1x
 
 CREATE TABLE Clientes(
-id_cliente INT PRIMARY KEY NOT NULL,
+id_cliente VARCHAR(36) PRIMARY KEY DEFAULT(UUID()),
 nombre VARCHAR(50),
 apellido VARCHAR(50),
 telefono VARCHAR(10),
@@ -11,7 +11,7 @@ direccion VARCHAR(255)
 );
 
 CREATE TABLE empleados(
-id_empleado INT PRIMARY KEY NOT NULL,
+id_empleado VARCHAR(36) PRIMARY KEY DEFAULT(UUID()),
 nombre VARCHAR(50),
 apellido VARCHAR(20), 
 cargo VARCHAR(50),
@@ -19,40 +19,36 @@ fecha_contratacion DATE,
 salario DECIMAL(10.2)
 );
 
-
 CREATE TABLE pedidos(
-id_pedido INT PRIMARY KEY NOT NULL,
+id_pedido VARCHAR(36) PRIMARY KEY DEFAULT(UUID()),
 fercha_pedido DATE, 
 total DECIMAL(10.2),
 estado VARCHAR(20),
-id_empleado INT,
-id_cliente INT,
+id_empleado VARCHAR(36),
+id_cliente VARCHAR(36),
 CONSTRAINT fk_cl FOREIGN KEY(id_cliente) REFERENCES Clientes(id_cliente),
 CONSTRAINT fk_em FOREIGN KEY(id_empleado) REFERENCES empleados(id_empleado)
 );
 
 CREATE TABLE productos(
-id_producto INT PRIMARY KEY NOT NULL,
+id_producto VARCHAR(36) PRIMARY KEY DEFAULT(UUID()),
 nombre VARCHAR(50),
 descripcion VARCHAR(20), 
 precio DECIMAL(10.2),
 existencias INT,
-id_pedido INT,
-id_detalle INT
+id_pedido VARCHAR(36)
 );
 
+
 CREATE TABLE detallePedido(
-id_detalle INT PRIMARY KEY NOT NULL,
+id_detalle VARCHAR(36) PRIMARY KEY DEFAULT(UUID()),
 cantidad INT,
 precio_unitario DECIMAL(10.2),
 subtotal DECIMAL(10.2),
-id_pedido INT,
-id_producto INT, 
+id_pedido VARCHAR(36),
+id_producto VARCHAR(36), 
 CONSTRAINT fk_pedid FOREIGN KEY(id_pedido) REFERENCES pedidos(id_pedido),
 CONSTRAINT fk_produ FOREIGN KEY(id_producto) REFERENCES productos(id_producto)
 );
-
-
-
 
 
